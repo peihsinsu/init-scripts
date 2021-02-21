6ee3c0c3c6b2b249d083235b68725adccb3d6f57
Simon Su <simonsu.mail@gmail.com>
2021-02-21 15:35:50 +0800

    Update percona-cluster.sh

--- mysql/percona-cluster.sh
+++ mysql/percona-cluster.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-# ref: https://www.percona.com/doc/percona-repo-config/percona-release.html#debian-and-ubuntu
-sudo apt-get install -y wget gnupg2 lsb-release curl
-wget https://repo.percona.com/apt/percona-release_latest.generic_all.deb
-sudo dpkg -i percona-release_latest.generic_all.deb
+# ref: 
+# https://www.percona.com/doc/percona-repo-config/percona-release.html#debian-and-ubuntu
+# https://www.databasesandlife.com/how-to-install-percona-mysql-5-6-on-ubuntu-14-04-lts-from-a-script-i-e-without-an-interactive-prompt-for-the-mysql-root-password/
+
+echo "Start install percina cluster..."
+sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
+sudo bash -c 'echo deb http://repo.percona.com/apt trusty main >> /etc/apt/sources.list'
+sudo bash -c 'echo deb-src http://repo.percona.com/apt trusty main >> /etc/apt/sources.list'
 sudo apt-get update
-sudo rm percona-release_latest.$(lsb_release -sc)_all.deb
 
 echo "NOTE: Remember to remove apparmor or related package..."
-
 echo "Install XDB server..."
-sudo apt-get install percona-xtradb-cluster -y
+echo "percona-server-server-5.6 percona-server-server/root_password password root" | sudo debconf-set-selections
+echo "percona-server-server-5.6 percona-server-server/root_password_again password root" | sudo debconf-set-selections
+sudo apt-get install -qq -y percona-server-server-5.6 percona-server-client-5.6
 
 echo "Stop server and wait for join..."
 sudo service mysql stop
 
-
+echo "End of installation..."
