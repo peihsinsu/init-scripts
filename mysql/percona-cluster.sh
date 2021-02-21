3678410f939e08136ed092da5ab1a5ae78064cd1
Simon Su <simonsu.mail@gmail.com>
2021-02-22 00:19:52 +0800

    Update percona-cluster.sh

--- mysql/percona-cluster.sh
+++ mysql/percona-cluster.sh
@@ -7,16 +7,19 @@ echo "Start install percona-xtradb-cluster-full-57..."
 #sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
 #sudo bash -c 'echo deb http://repo.percona.com/apt trusty main >> /etc/apt/sources.list'
 #sudo bash -c 'echo deb-src http://repo.percona.com/apt trusty main >> /etc/apt/sources.list'
-sudo apt-get install -y wget vim gnupg2 lsb-release curl
-wget wget https://repo.percona.com/apt/percona-release_latest.generic_all.deb
-sudo dpkg -i percona-release_latest.generic_all.deb
-sudo apt-get update
+apt-get update
+apt-get install -y wget vim gnupg2 lsb-release curl
+wget https://repo.percona.com/apt/percona-release_latest.generic_all.deb
+dpkg -i percona-release_latest.generic_all.deb
+apt-get update
 
 echo "NOTE: Remember to remove apparmor or related package..."
 echo "Install XDB server..."
-#echo "percona-xtradb-cluster-server-5.7 percona-xtradb-cluster-server-5.7/root_password password root" | sudo debconf-set-selections
-#echo "percona-xtradb-cluster-server-5.7 percona-xtradb-cluster-server-5.7/root_password_again password root" | sudo debconf-set-selections
-sudo apt-get install -qq -y percona-xtradb-cluster-server-5.7
+echo "percona-xtradb-cluster-server-5.7 percona-xtradb-cluster-server-5.7/root_password password root" |  debconf-set-selections
+echo "percona-xtradb-cluster-server-5.7 percona-xtradb-cluster-server-5.7/root_password_again password root" |  debconf-set-selections
+echo "percona-xtradb-cluster-server-5.7 percona-xtradb-cluster-server-5.7/root-pass password root" |  debconf-set-selections
+echo "percona-xtradb-cluster-server-5.7 percona-xtradb-cluster-server-5.7/re-root-pass password root" |  debconf-set-selections
+apt-get install -qq -y percona-xtradb-cluster-server-5.7
 
 echo "Stop server and wait for join..."
 sudo service mysql stop
